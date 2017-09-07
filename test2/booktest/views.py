eea0c0f6827f3547c760dc2e55f21515cd0dc77b
linkkuyu <linkkuyu@gmail.com>
2017-09-07 18:33:57 +0800

    查询

--- test2/booktest/views.py
+++ test2/booktest/views.py
@@ -1,3 +1,17 @@
+#coding:utf-8
 from django.shortcuts import render
+from models import *
+from django.db.models import Max, F
 
 # Create your views here.
+def index(request):
+    # list = BookInfo.books1.filter(heroinfo__hcontent__contains='六')
+    # list = BookInfo.books1.filter(pk__lte=3)
+    # Max1 = BookInfo.books1.aggregate(Max('bput_date'))
+    # list = BookInfo.books1.filter(bread__gt=F('bcommet'))
+    # list = BookInfo.books1.filter(pk__lt=4, btitle__contains='1')
+    list = BookInfo.books1.filter(pk__lt=4, btitle__contains='1')
+    context = {'list1':list
+               # 'Max1':Max1
+               }
+    return render(request, 'booktest/index.html', context)
