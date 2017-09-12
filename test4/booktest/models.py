dde3ff3186850c668357e418c09e4db1199265a0
linkkuyu <linkkuyu@gmail.com>
2017-09-12 22:17:29 +0800

    test4

--- test4/booktest/models.py
+++ test4/booktest/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class BookInfo(models.Model):
-    btitle = models.CharField(max_lenght=20)
-    bpub_date = models.DateTimeField(db_column='[pub_date]')
+    btitle = models.CharField(max_length=20)
+    bpub_date = models.DateTimeField(db_column='pub_date')
     bread = models.IntegerField()
     bcommet = models.IntegerField()
-    isDelete = models.BooleanField()
+    isDelet = models.BooleanField()
     class Meta():
         db_table = 'bookinfo'
 
@@ -13,5 +13,7 @@ class HeroInfo(models.Model):
     hname = models.CharField(max_length=10)
     hgender = models.BooleanField()
     hcontent = models.CharField(max_length=1000)
-    isDelete = models.BooleanField()
-    hero = models.ForeignKey('BookInfo')
+    isDelet = models.BooleanField()
+    book = models.ForeignKey('BookInfo')
+    def showname(self):
+        return self.hname
--- test4/booktest/views.py
+++ test4/booktest/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from models import *
 
 def index(request):
-    context = {}
-    return render(request, 'booktest/index.html')
+    hero = HeroInfo.objects.get(pk=1)
+    context = {'hero':hero}
+    return render(request, 'booktest/index.html', context)
