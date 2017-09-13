9eb4421dd09aeb45c3f8c8131f8fae521e5f8a41
linkkuyu <linkkuyu@gmail.com>
2017-09-13 18:21:01 +0800

    templates

--- test4/booktest/urls.py
+++ test4/booktest/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 import views
 
-urlpatterns = {
-    url(r'^$', views.index, name='index')
-}
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^(\d+)/(\d+)$',views.show, name='show'),
+    url(r'^index2', views.index2, name='index2')
+
+]
--- test4/booktest/views.py
+++ test4/booktest/views.py
@@ -1,7 +1,18 @@
+#coding:utf-8
 from django.shortcuts import render
 from models import *
 
 def index(request):
-    hero = HeroInfo.objects.get(pk=1)
-    context = {'hero':hero}
+    # hero = HeroInfo.objects.get(pk=1)
+    # context = {'hero':hero}
+    list = HeroInfo.objects.filter(isDelet=False)
+    context = {'list':list}
     return render(request, 'booktest/index.html', context)
+
+def show(request, id, id2):
+    context = {'id':id}
+    return render(request,'booktest/show.html', context)
+
+#练习模板的继承
+def index2(request):
+    return render(request, 'booktest/index2.html')
